c16742e9da01cd0f429d78f3faaaf92814ac1839
BriYuDev <aesblox12@gmail.com>
2022-12-28 12:05:52 +0700

    changes

--- src/assets/Art1/index.js
+++ src/assets/Art1/index.js
@@ -1,9 +1,9 @@
-import art11 from './Art1-1.jpeg'
-import art12 from './Art1-2.jpeg'
-import art13 from './Art1-3.jpeg'
-import art14 from './Art1-4.jpeg'
-import art15 from './Art1-5.jpeg'
-import art16 from './Art1-6.jpeg'
+import art11 from './Art1-1.webp'
+import art12 from './Art1-2.webp'
+import art13 from './Art1-3.webp'
+import art14 from './Art1-4.webp'
+import art15 from './Art1-5.webp'
+import art16 from './Art1-6.webp'
 
 export {
     art11,
