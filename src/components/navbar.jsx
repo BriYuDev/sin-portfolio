3ca68946efadf186ba636497977275536d145257
BriYuDev <aesblox12@gmail.com>
2022-12-28 13:24:06 +0700

    new changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,16 @@ import { logo } from '../assets';
 import { NavLinks } from '../constants';
 
 class Navbar extends Component {
-  state = {};
+  state = {
+    menuvalue: false,
+  };
+
+  changemenu = () => {
+    let menuvalue = this.state.menuvalue;
+    menuvalue = !menuvalue;
+    this.setState({ menuvalue });
+  };
+
   render() {
     return (
       <div className='flex flex-row justify-between items-center z-[5]'>
@@ -21,6 +30,36 @@ class Navbar extends Component {
                 key={link.id}
                 className={`${
                   index !== NavLinks.length - 1 ? 'mr-[25px]' : 'mr-0'
+                } font-poppins font-[500] text-[20px] nav-text`}
+              >
+                <a href={link.id}>{link.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div
+          onClick={this.changemenu}
+          className={`${
+            this.state.menuvalue === false ? 'closed' : 'opened'
+          } flex flex-col md:hidden justify-center items-center`}
+        >
+          <div className='w-[55px] h-[4px] navmenu-color line-first relative' />
+          <div className='w-[55px] h-[4px] navmenu-color mt-[8px] line-second relative' />
+          <div className='w-[55px] h-[4px] navmenu-color mt-[8px] line-third relative' />
+        </div>
+
+        <div
+          className={`${
+            this.state.menuvalue === false ? 'closed-menu' : 'opened-menu'
+          } md:hidden absolute top-[120%] right-5 flex flex-col h-[100px] w-[100px] items-center justify-center menu-color rounded-[10px]`}
+        >
+          <ul className='list-none flex flex-col items-center justify-around'>
+            {NavLinks.map((link, index) => (
+              <li
+                key={link.id}
+                className={`${
+                  index !== NavLinks.length - 1 ? 'mb-[5px]' : 'mb-0'
                 } font-poppins font-[500] text-[20px] nav-text`}
               >
                 <a href={link.id}>{link.title}</a>
